079549d2bb9ae34a99bbf78fea480c96d36ee886
sathish19961412 <sathishfreelanc5@gmail.com>
2023-12-05 17:25:45 +0530

    completed crud operation

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from myapp.form import RegisterForm
+from myapp.models import Data
+from django.contrib import messages
 # Create your views here.
 
 def home(request):
     myform=RegisterForm()
-    return render(request,'index.html',{'forms':myform})
+    mydata=Data.objects.all()
+    if mydata!='':
+        return render(request,'index.html',{'forms':myform,'datas':mydata})
+    else:
+        render(request,'index.html',{'forms':myform})
+
+def addData(request):
+    if request.method=='POST':
+        myform=RegisterForm(request.POST)
+        if myform.is_valid():
+           myform.save()
+           messages.success(request,"Record Added Successfully..!!")
+           return redirect('Home')
+        
+def updateData(request,id):
+    mydata=Data.objects.get(id=id)
+    myform=RegisterForm(instance=mydata)
+    if request.method=='POST':
+        myform=RegisterForm(request.POST,instance=mydata)
+        if myform.is_valid():
+           myform.save()
+           messages.success(request,"Record Update Successfully..!!")
+           return redirect('Home')
+    context={'forms':myform}
+    return render(request,'update.html',context)
+
+def deleteData(request,id):
+    mydata=Data.objects.get(id=id)
+    mydata.delete()
+    messages.success(request,"Record Delete Successfully..!!")
+    return redirect('Home')
+
--- myproject/urls.py
+++ myproject/urls.py
@@ -21,4 +21,7 @@ from myapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='Home'),
+    path('addData',views.addData,name='addData'),
+    path('updateData/<int:id>',views.updateData,name='updateData'),
+    path('deleteData/<int:id>',views.deleteData,name='deleteData'),
 ]
